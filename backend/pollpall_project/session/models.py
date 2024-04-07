3e4d77f86e36296f7f903d6add9312bd52a76c70
Damien Flutre <damien.flutre@gmail.com>
2024-04-07 13:16:15 -0700

    poll and answers progress commit

--- backend/pollpall_project/session/models.py
+++ backend/pollpall_project/session/models.py
@@ -1,8 +1,19 @@
 from django.db import models
 
+
 class Session(models.Model):
     session_id = models.IntegerField(primary_key=True)
     session_label = models.CharField(max_length=255)
 
     def __str__(self):
         return f"{self.session_id}: {self.session_label}"
+
+class Poll(models.Model):
+    poll_id = models.IntegerField(primary_key=True)
+    poll = models.CharField(max_length=255)
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+
+class Answer(models.Model):
+    question = models.ForeignKey(Poll, on_delete=models.CASCADE)
+    question_text = models.CharField(max_length=255)
+    num_votes = models.IntegerField(default=0)
--- backend/pollpall_project/session/views.py
+++ backend/pollpall_project/session/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from .models import Session
+from .models import Session, Poll, Answer
 from .serializers import SessionSerializer
 
 class SessionListCreate(generics.ListCreateAPIView):
@@ -11,3 +11,5 @@ class SessionDestroy(generics.DestroyAPIView):
     queryset = Session.objects.all()
     serializer_class = SessionSerializer
     lookup_field = 'session_id'
+
+class PollCreate()
