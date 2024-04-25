58c5618a6544acc8cd435cb42deb6d369bc97bf1
Damien Flutre <damien.flutre@gmail.com>
2024-04-24 18:22:01 -0700

    comment push

--- backend/pollpall_project/session/models.py
+++ backend/pollpall_project/session/models.py
@@ -18,5 +18,5 @@ class Question(models.Model): # Just holds a question ID and the question
 class Response(models.Model): # Holds a response and whether it is the correct answer or not.
     response_id = models.IntegerField(primary_key=True)
     prompt = models.CharField(max_length=0xFFF)
-    question = models.ForeignKey(Question, on_delete=models.CASCADE) # If question is deleted, delete the responses attached to it.
+    question = models.ForeignKey(Question, on_delete=models.CASCADE) # Connects Response to Question.
     is_correct = models.BooleanField(default=False)
