5f83f30f112270dc769876d0bc883638a5ed95cb
Damien Flutre <damien.flutre@gmail.com>
2024-04-24 19:26:17 -0700

    Code for models and views. Not production ready.

--- backend/pollpall_project/session/models.py
+++ backend/pollpall_project/session/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from .models import Question
 
 class Session(models.Model):
     session_id = models.IntegerField(primary_key=True)
@@ -7,10 +8,21 @@ class Session(models.Model):
     def __str__(self):
         return f"{self.session_id}: {self.session_label}"
 
+# TODO
+# By using ForeignKey, each question should be automatically connected to the poll
+# that fetches it (Not figured out). Each response is also connected to it's respective
+# question, so we shouldn't need a list of responses.
+class Poll(models.Model):
+    poll_id = models.IntegerField(primary_key=True) 
+    session = models.ForeignKey(Session, on_delete=models.CASCADE)
+    question_id = models.IntegerField(default=-1)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    is_accepting_answers = models.BooleanField(default=False)
+
 class Question(models.Model): # Just holds a question ID and the question
     question_id = models.IntegerField(primary_key=True) 
     prompt = models.CharField(max_length=0xFFF)
-    session = models.ForeignKey(Session, on_delete=models.CASCADE) # If session is deleted, also delete question from database.
+    poll = models.ForeignKey(Poll, on_delete=models.CASCADE) # If session is deleted, also delete question from database.
     
     def __str__(self):
         return f"{self.question_id}: {self.prompt}: {self.session}"
--- backend/pollpall_project/session/views.py
+++ backend/pollpall_project/session/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from .models import Session
+from .models import Session, Question, Response, Poll
 from .serializers import SessionSerializer
 
 class SessionListCreate(generics.ListCreateAPIView):
@@ -11,3 +11,72 @@ class SessionDestroy(generics.DestroyAPIView):
     queryset = Session.objects.all()
     serializer_class = SessionSerializer
     lookup_field = 'session_id'
+
+# TODO
+class PollService():
+    
+    # Clean these up
+    
+    @staticmethod
+    def nextQuestion(poll_id, question):
+        try:
+            poll = Poll.objects.get(id=poll_id)
+            poll.question = question
+            poll.save()
+            return True
+        except Poll.DoesNotExist:
+            return False
+    
+    @staticmethod
+    def haltReceivingAnswers(poll_id):
+        try:
+            poll = Poll.objects.get(id=poll_id)
+            poll.is_accepting_answers = False
+            poll.save()
+            return True
+        except Poll.DoesNotExist:
+            return False
+    
+    @staticmethod
+    def getAnswer(poll_id):
+        try:
+            poll = Poll.objects.get(id=poll_id)
+            if not poll.is_accepting_answers:
+                # Get poll CORRECT answer
+                return poll.answer
+            else:
+                return None
+        except Poll.DoesNotExist:
+            return None
+
+    @staticmethod
+    def submitResponse(poll_id, response):
+        try:
+            poll = Poll.objects.get(id=poll_id)
+            poll_response = Response.objects.create(poll=poll, response=response)
+            poll_response.save()
+            return True
+        except Poll.DoesNotExist:
+            return False
+
+
+# TODO
+class SessionService():
+    @staticmethod
+    def startSession(session_id, initial_question):
+        try:
+            session = Session.objects.get(id=session_id)
+            poll = Poll.objects.create(session=session, current_question=initial_question)
+            poll.save()
+            return True
+        except Session.DoesNotExist:
+            return False
+    
+    @staticmethod
+    def endSession(session_id):
+        try:
+            poll = Poll.objects.get(session__id=session_id)
+            poll.delete()
+            return True
+        except Poll.DoesNotExist:
+            return False
