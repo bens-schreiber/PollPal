bedd77a030cb0dc061069da25862faa1c03f9a5a
DamiennFl <damien.flutre@gmail.com>
2024-04-26 13:15:14 -0700

    feedback

--- backend/pollpall_project/session/models.py
+++ backend/pollpall_project/session/models.py
@@ -7,11 +7,17 @@ class Session(models.Model):
     def __str__(self):
         return f"{self.session_id}: {self.session_label}"
 
-# TODO
-# By using ForeignKey, each question should be automatically connected to the poll
-# that fetches it. Each response is also connected to it's respective
-# question, so we shouldn't need a list of responses.
 class Poll(models.Model):
+    """
+    Holds the current poll
+    
+    Attributes:
+        poll_id (int): Primary key of model
+        session (Session): Session which this poll is connected to
+        question_id (int): Integer ID of current question
+        related_question (Question): Current question
+        is_accepting_answers (bool): Whether the poll is accepting and updating answers.
+    """
     poll_id = models.IntegerField(primary_key=True) 
     session = models.ForeignKey('Session', on_delete=models.CASCADE)
     question_id = models.IntegerField(default=-1)
@@ -21,19 +27,35 @@ class Poll(models.Model):
     def __str__(self):
         return f"{self.poll_id}: {self.session}: {self.question_id}: {self.related_question}: {self.is_accepting_answers}"
 
-class Question(models.Model): # Just holds a question ID and the question
+class Question(models.Model):
+    """ 
+    Holds the question we are asking 
+    
+    Attributes:
+        question_id (int): The primary key of the model
+        prompt (str): The question we are asking
+        poll (Poll): The poll this question comes from
+    """
     question_id = models.IntegerField(primary_key=True) 
     prompt = models.CharField(max_length=0xFFF, default = '')
-    related_poll = models.ForeignKey('Poll', on_delete=models.CASCADE, related_name='related_questions') # If poll is deleted, also delete question from database.
+    poll = models.ForeignKey('Poll', on_delete=models.CASCADE, related_name='related_questions')
     
     def __str__(self):
         return f"{self.question_id}: {self.prompt}: {self.related_poll}"
 
-class Response(models.Model): # Holds a response and whether it is the correct answer or not.
+class Response(models.Model):
+    """
+    Holds a response to the linked question
+    
+    Attributes:
+        response_id (int): Primary key of the model
+        prompt (str): The response
+        question (Question): The question this response comes from
+        is_correct (bool): Whether the question is true or not
+    """
     response_id = models.IntegerField(primary_key=True)
     prompt = models.CharField(max_length=0xFFF, default = '')
-    # Foreign key should cover One-to-Many connection
-    question = models.ForeignKey('Question', on_delete=models.CASCADE) # Connects Response to Question.
+    question = models.ForeignKey('Question', on_delete=models.CASCADE)
     is_correct = models.BooleanField(default=False)
     
     def __str__(self):
