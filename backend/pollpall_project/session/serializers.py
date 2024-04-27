010542258bc43dbf6f561068534f6a7381b77b09
dsherli <dillon.sherling@wsu.edu>
2024-04-26 18:12:40 -0700

    formatted

--- backend/pollpall_project/session/serializers.py
+++ backend/pollpall_project/session/serializers.py
@@ -6,22 +6,24 @@ from rest_framework.response import Response
 class SessionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Session
-        fields = ("__all__")
+        fields = "__all__"
 
     lookup_field = "session_id"
+
 
 class PollSerializer(serializers.ModelSerializer):
     class Meta:
         model = Poll
-        fields = ("__all__")
-    
+        fields = "__all__"
+
+
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
         model = Question
-        fields = ("__all__")
+        fields = "__all__"
 
-    
+
 class ResponseSerializer(serializers.ModelSerializer):
     class Meta:
         model = Response
-        fields = ("__all__")
+        fields = "__all__"
--- backend/pollpall_project/session/views.py
+++ backend/pollpall_project/session/views.py
@@ -13,4 +13,3 @@ class SessionDestroy(generics.DestroyAPIView):
     queryset = Session.objects.all()
     serializer_class = SessionSerializer
     lookup_field = "session_id"
-
