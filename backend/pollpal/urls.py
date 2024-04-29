a52e6b30f1f5483fcd864d0776be388edae270cb
Ben Schreiber <bpschreiber2003@gmail.com>
2024-04-28 18:31:52 -0700

    format

--- backend/pollpal/urls.py
+++ backend/pollpal/urls.py
@@ -23,7 +23,7 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("api/", include("session.urls")),
-    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/schema/swagger-ui/",
         SpectacularSwaggerView.as_view(url_name="schema"),
