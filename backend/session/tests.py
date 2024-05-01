7084b3d67616df72ada207cac504de168c88191a
Damien Flutre <damien.flutre@gmail.com>
2024-04-30 21:22:31 -0700

    began making endSession tests

--- backend/session/tests.py
+++ backend/session/tests.py
@@ -16,6 +16,7 @@ class TestSessionService(TestCase):
 
         # Arrange
         session = Session.objects.create(label="Session")
+        session.save()
 
         question = Question.objects.create(prompt="Test Starting Session")
         answer = Answer.objects.create(
@@ -36,3 +37,43 @@ class TestSessionService(TestCase):
         self.assertEqual(poll.session.id, session.id)
         self.assertTrue(poll.is_accepting_answers)
         self.assertIsNotNone(poll.question)
+
+    def test_endSession_deleteNullPoll__returnsError(self):
+        # Arrange
+        session = Session.objects.create(label="Session")
+        session.save()
+
+        data = {"session": SessionSerializer(session).data}
+
+        # Act
+        response = self.client.delete(
+            self.sessionManagerURL, json.dumps(data), content_type="application/json"
+        )
+
+        # Assert
+        self.assertEqual(response.status_code, 400)
+
+    def test_endSession_deletePoll_pollDeleted(self):
+        # Arrange
+        session = Session.objects.create(label="Session")
+        session.save()
+
+        poll = Poll.objects.create(prompt="Test Poll", session=session)
+        poll.save()
+
+        data = {
+            "session": SessionSerializer(session).data,
+            "poll": PollSerializer(poll).data,
+        }
+
+        # Act
+        response = self.client.delete(
+            self.sessionManagerURL, json.dumps(data), content_type="application/json"
+        )
+
+        # Assert
+        self.assertEqual(response.status_code, 202)
+        self.assertIsNone(len(list(Poll.objects.all)))
+        self.assertIsNone(len(list(Question.objects.all)))
+        self.assertIsNone(len(list(Response.objects.all)))
+        self.assertIsNone(len(list(Answer.objects.all)))
