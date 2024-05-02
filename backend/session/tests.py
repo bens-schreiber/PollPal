d4d492aa286aa995fe2229dfb27a283269769456
Ben Schreiber <bpschreiber2003@gmail.com>
2024-05-02 13:18:01 -0700

    changed how we approach APIViews

--- backend/session/tests.py
+++ backend/session/tests.py
@@ -10,29 +10,32 @@ from .serializers import *
 class TestSessionService(TestCase):
     def setUp(self):
         self.client = Client()
-        self.sessionManagerURL = reverse("pollpal:session-manage")
+        self.sessionStartURL = reverse("pollpal:session-start")
 
     def test_startSession_create_validPoll(self):
 
         # Arrange
-        session = Session.objects.create(label="Session")
-
-        question = Question.objects.create(prompt="Test Starting Session")
+        session = Session.objects.create(label="Session 1")
+        question = Question.objects.create(prompt="Question 1")
         answer = Answer.objects.create(
-            answer="Test answer", question=question, is_correct=True
+            answer="Answer 1", question=question, is_correct=True
         )
-        data = {
-            "session": SessionSerializer(session).data,
-            "question": QuestionSerializer(question).data,
-        }
+
+        session.save()
+        question.save()
+        answer.save()
+
+        data = {"session": session.id, "question": question.id}
 
         # Act
         response = self.client.post(
-            self.sessionManagerURL, json.dumps(data), content_type="application/json"
+            self.sessionStartURL, json.dumps(data), content_type="application/json"
         )
+
         # Assert
         self.assertEqual(response.status_code, 201)
-        poll = Poll.objects.get(pk=response.data["id"])
+        poll: Poll = Poll.objects.get(pk=response.data["id"])
+
         self.assertEqual(poll.session.id, session.id)
+        self.assertEqual(poll.question.id, question.id)
         self.assertTrue(poll.is_accepting_answers)
-        self.assertIsNotNone(poll.question)
